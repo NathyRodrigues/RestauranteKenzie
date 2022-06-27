5331ffcfaaa7c84b3bbd47f1bbe612d8d2885f8e
Nathy <nathalia.r.s@outlook.com>
2022-06-27 17:34:46 -0300

    adicionando arquivos javascript

---
+++ Restaurante/assets/JS/database.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    id: "01",
+    title: "Kenzie Burgão",
+    description: "O hambúrguer do Dev Full Stack Kenzie, o mais completo da cidade!",
+    price: "28.00",
+    img: "./Restaurante/assets/img/kenzie_burgao.png",
+  },
+  {
+    id: "02",
+    title: "Refrigerante",
+    description: "A melhor bebida do mundo, geladíssima e refrescante!",
+    price: "7.50",
+    img: "./Restaurante/assets/img/refrigerante.png" ,
+  },
+  {
+    id: "03",
+    title: "Kenzie Burguinho",
+    description:
+      "Bacon, gema de ovo mole especial e muito sabor pra você!",
+    price: "27.50",
+    img: "./Restaurante/assets/img/kenzie_burguinho.png",
+  },
+  {
+    id: "04",
+    title: "Frango Frito",
+    description: "Frango crocante e empanado acompanhado de molho incrível da casa.",
+    price: "32.50",
+    img: "./Restaurante/assets/img/frango_frito.png",
+  },
+  {
+    id: "05",
+    title: "Batata Frita",
+    description: "A batata mais sequinha da região, acompanha cheddar e bacon.",
+    price: "15.00",
+    img: "./Restaurante/assets/img/batata_frita.png",
+  },
+  {
+    id: "06",
+    title: "Sanduíche com Frango Frito",
+    description: "Irresistível empanado com salada fresca e molho especial.",
+    price: "23.50",
+    img: "./Restaurante/assets/img/sanduiche_frango_frito.png",
+  },
+];
